aa341676aafc0ff0a524da320d6e8e408352f035
pitscher <mail@hn-goering.de>
2020-07-26 18:36:17 +0200

    Add ssh based code deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+sshpass -p "$OVH_HOSTING_PASSWORD" ssh -o StrictHostKeyChecking=no $OVH_HOSTING_USER@$OVH_HOSTING_DOMAIN "rm -rv * && git clone $REPOSITORY_URL && mv $REPOSITORY_NAME/* . && rm -rf $REPOSITORY_NAME"
