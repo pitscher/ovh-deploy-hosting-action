d4be2acb676f772475dbd51f4e7c434fb82c1b00
pitscher <mail@hn-goering.de>
2020-07-27 11:29:21 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
-#!/bin/sh -l
+#!/bin/bash
 
 sshpass -p "$OVH_HOSTING_PASSWORD" ssh -o StrictHostKeyChecking=no $OVH_HOSTING_USER@$OVH_HOSTING_DOMAIN "rm -rv * && git clone $REPOSITORY_URL && mv $REPOSITORY_NAME/* . && rm -rf $REPOSITORY_NAME"
