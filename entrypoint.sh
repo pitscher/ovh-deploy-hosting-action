eafad750dce0a995fe7016d3617f7f912b02a86d
pitscher <mail@hn-goering.de>
2020-07-27 15:37:41 +0200

    Add sftp support

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,15 @@ if [[ -z $OVH_HOSTING_USER || -z $OVH_HOSTING_PASSWORD || -z $OVH_HOSTING_DOMAIN
   exit 1
 fi
 
-sshpass -p "$OVH_HOSTING_PASSWORD" ssh -o StrictHostKeyChecking=no $OVH_HOSTING_USER@$OVH_HOSTING_DOMAIN "rm -rv * && git clone $REPOSITORY_URL && mv $REPOSITORY_NAME/* . && mv $REPOSITORY_NAME/.htaccess . && rm -rf $REPOSITORY_NAME && rm README.md LICENSE"
+expect -c "
+  spawn sftp -o "BatchMode=no" "$OVH_HOSTING_USER@$OVH_HOSTING_DOMAIN"
+  expect -nocase \"*password:\" { send \"$OVH_HOSTING_PASSWORD\r\"; interact }"
+
+rm -rv *
+git clone $REPOSITORY_URL
+mv $REPOSITORY_NAME/* .
+mv $REPOSITORY_NAME/.htaccess
+rm -rf $REPOSITORY_NAME README.md LICENSE
+exit 0
+
+#sshpass -p "$OVH_HOSTING_PASSWORD" ssh -o StrictHostKeyChecking=no $OVH_HOSTING_USER@$OVH_HOSTING_DOMAIN "rm -rv * && git clone $REPOSITORY_URL && mv $REPOSITORY_NAME/* . && mv $REPOSITORY_NAME/.htaccess . && rm -rf $REPOSITORY_NAME && rm README.md LICENSE"
